34e6723c50be3b22609d46a1f877be5b6b7ecbd7
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-12-20 09:23:51 +0100

    Create fibs_rec method with help

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -19,3 +19,13 @@ p fibs(0)
 p fibs(1)
 p fibs(2)
 p fibs(8)
+
+def fibs_rec(n)
+  return [] if n == 0
+  return [0] if n == 1
+  return [0, 1] if n ==2
+  seq = fibs_rec(n-1)
+  seq.push(seq[-2] + seq[-1])
+end
+
+p fibs_rec(8)
